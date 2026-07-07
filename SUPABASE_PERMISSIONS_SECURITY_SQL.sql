1a212d7f8213c93688b406afad4e0f3b2cbd5824
dr_m_aldemerdash <sa.alnour.ms@gmail.com>
2026-07-07 10:13:41 +0300

    ٠٠

--- SUPABASE_PERMISSIONS_SECURITY_SQL.sql
+++
@@ -1,9 +0,0 @@
-alter table app_user_profiles
-add column if not exists employee_id text;
-
-alter table app_user_profiles
-add column if not exists permissions jsonb default '{}'::jsonb;
-
-update app_user_profiles
-set permissions = '{}'::jsonb
-where permissions is null;
